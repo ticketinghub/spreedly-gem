99b23198efb3396b96197769345141da44fac70b
pawel-krysiak <pawelkrysiak@hotmail.com>
2019-04-18 18:53:39 +0200

    refactored and fixed bug.

--- lib/spreedly/signature_validator.rb
+++ lib/spreedly/signature_validator.rb
@@ -6,23 +6,21 @@ module Spreedly
     def initialize(secret, raw_xml_response)
       @secret = secret
       @raw_xml_response = raw_xml_response
+      @document = Nokogiri::XML(raw_xml_response)
     end
 
     def valid?
-      signature = document.xpath("//transaction/signed/signature").text
-      signature == signature_for(@secret, @raw_xml_response)
+      signature = @document.xpath("//transaction/signed/signature").text
+      signature == signature_for(@secret)
     end
 
-    private
+    def signature_for(secret)
+      algorithm = @document.xpath("//transaction/signed/algorithm").text
 
-    def signature_for(secret, xml)
-      document = Nokogiri::XML(xml)
-      algorithm = document.xpath("//transaction/signed/algorithm").text
-
-      fields = document.xpath("//transaction/signed/fields").text.split(" ")
+      fields = @document.xpath("//transaction/signed/fields").text.split(" ")
 
       values = fields.collect do |field|
-        document.xpath("//transaction/#{field}").text
+        @document.xpath("//transaction/#{field}").text
       end
 
       signature_data = values.join("|")
